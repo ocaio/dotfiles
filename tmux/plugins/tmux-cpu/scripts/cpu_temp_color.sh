df54d4dc302d7613d68c2aaa8704ab6fb5021024
Caio Elias <caiom.elias@gmail.com>
2020-09-22 19:45:35 -0300

    tmux: add cpu_temp scripts

---
+++ tmux/plugins/tmux-cpu/scripts/cpu_temp_color.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+source "$CURRENT_DIR/helpers.sh"
+print_cpu_temp() {
+  if command_exists "sensors"; then
+    sensors | sed '/^[^Package]/d' | sed '/^\s*$/d' | head -n 2 | tail -n 1 | awk '{a=$4} END {printf("%5.0fºC", a)}' | sed 's/ //g'
+  else
+    echo "N/A"
+  fi
+}
+
+main() {
+  print_cpu_temp
+}
+main
